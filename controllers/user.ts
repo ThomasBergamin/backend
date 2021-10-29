0629082566b911765dfd5a620a8872d4f7576034
Thomas <thomas.bergamin@placemeet.fr>
2021-10-29 11:00:24 +0200

    feat: 🎸 create signup route

--- controllers/user.ts
+++ controllers/user.ts
@@ -1,10 +1,41 @@
+import { NextFunction, Request, Response } from "express";
 import { db } from "../models";
 import { User } from "../models/User";
+import bcrypt from "bcrypt";
 
-export const createUser = (req: any, res: any, next: any) => {
-  db.sync().then(() =>
-    User.create({ username: "janedoe" })
-      .then(res.status(200).json({ message: "User created" }))
-      .catch((error) => res.status(500).json({ error: "Error creating user" }))
-  );
+export const signup = (
+  req: {
+    body: {
+      lastName: string;
+      email: string;
+      firstName: string;
+      password: string;
+    };
+  },
+  res: Response,
+  next: NextFunction
+) => {
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) =>
+      db.sync().then(async () => {
+        await User.create({
+          lastName: req.body.lastName,
+          firstName: req.body.firstName,
+          password: hash,
+          email: req.body.email,
+        })
+          .then(() =>
+            res.status(200).json({ message: "User successfully created" })
+          )
+          .catch((error) =>
+            res
+              .status(500)
+              .json({ message: "Error while creating user", error })
+          );
+      })
+    )
+    .catch((error) => res.status(500).json({ error }));
 };
+
+export const login = (req: Request, res: Response, next: NextFunction) => {};
--- routes/user.ts
+++ routes/user.ts
@@ -1,25 +1,6 @@
 import express from "express";
-import { createUser } from "../controllers/user";
-import { db } from "../models";
+import { signup } from "../controllers/user";
 
-export const router = express.Router();
+export const userRoutes = express.Router();
 
-router.post("/signup", async (req, res, next) => {
-  try {
-    await db.authenticate();
-    console.log(`Connection to database has been established successfully.`);
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-  res
-    .status(200)
-    .json({ hello: `Nice to meet you ${process.env.DB_USERNAME}` });
-});
-router.post("/login", (req, res, next) => {
-  res.status(200).json({ test: "Nice to meet you again" });
-});
-router.get("/test", (req, res, next) => {
-  res.status(200).json({ test: "Should work" });
-});
-
-router.get("/create", createUser);
+userRoutes.post("/signup", signup);
