ba50550eaf0d23835878a64c3e7ddf23c32ae992
Thomas <thomas.bergamin@placemeet.fr>
2021-11-03 16:17:07 +0100

    feat: 🎸 add more routes to Gifs

--- controllers/gif.ts
+++ controllers/gif.ts
@@ -31,3 +31,58 @@ export const createGif = (req: Request, res: Response, next: NextFunction) => {
     )
     .catch((error) => res.status(500).json({ error }));
 };
+
+export const getOneGif = (req: Request, res: Response, next: NextFunction) => {
+  Gif.findOne({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((gifs) => {
+      res.status(200).json(gifs);
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error,
+      });
+    });
+};
+
+export const deleteOneGif = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  Gif.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then(() => {
+      res.status(200).json({ message: "Gif successfully deleted" });
+    })
+    .catch((error) => {
+      res.status(400).json({ message: "Error while deleting Gif", error });
+    });
+};
+
+export const updateOneGif = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  Gif.update(
+    { authorId: req.body.authorId, title: req.body.title, url: req.body.url },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then(() => {
+      res.status(200).json({ message: "Gif successfully updated" });
+    })
+    .catch((error) => {
+      res.status(400).json({ message: "Error while updating Gif", error });
+    });
+};
--- routes/gif.ts
+++ routes/gif.ts
@@ -1,8 +1,17 @@
 import express from "express";
-import { getGifs, createGif } from "../controllers/gif";
+import {
+  getGifs,
+  createGif,
+  getOneGif,
+  deleteOneGif,
+  updateOneGif,
+} from "../controllers/gif";
 import { auth } from "../middlewares/auth";
 
 export const gifRoutes = express.Router();
 
 gifRoutes.get("/", auth, getGifs);
 gifRoutes.post("/", auth, createGif);
+gifRoutes.get("/:id", auth, getOneGif);
+gifRoutes.put("/:id", auth, updateOneGif);
+gifRoutes.delete("/:id", auth, deleteOneGif);
